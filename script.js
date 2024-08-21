789acae4362a9cb10ac2a09920a7a31716587d92
Aditya Silwal <silwala@sheridancollege.ca>
2024-08-21 03:43:55 -0400

    modal shows when add button is clicked and closed when close button is clicked

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const myLibrary = [];
 
-const dialog = document.querySelector("dialog");
+const modal = document.querySelector("#modal");
 const addButton = document.querySelector("#add-button");
+const closeModal = document.querySelector("#close-modal")
 
 addButton.addEventListener("click", () =>{
-    dialog.showModal();
+    modal.classList.add("active");
+})
+
+closeModal.addEventListener("click", () => {
+    modal.classList.remove("active");
 })
 
 function Book(title, author, pages, read){
