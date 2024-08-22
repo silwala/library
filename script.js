926c4f1be29d48e44e925808eb80828debe98d74
Aditya Silwal <silwala@sheridancollege.ca>
2024-08-22 03:59:44 -0400

    add books to array on clicking add book

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ const addButton = document.querySelector("#add-button");
 const closeModal = document.querySelector("#close-modal")
 const overlay = document.querySelector("#overlay")
 const library = document.querySelector("#library")
+const bookAddButton = document.querySelector("#form-add-button")
+
+const userTitle = document.querySelector("#title");
+const userAuthor = document.querySelector("#author");
+const userPages = document.querySelector("#pages");
+const userRead = document.querySelector("#read");
 
 addButton.addEventListener("click", () =>{
     modal.classList.add("active");
@@ -23,6 +29,17 @@ modalContainer.addEventListener("click", (e) => {
         overlay.classList.remove("active");
     }
 })
+
+bookAddButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    title = userTitle.value;
+    author = userAuthor.value;
+    pages = userPages.value;
+    read = (userRead.value === "on") ? true : false;
+
+    addBookToLibrary(new Book(title, author, pages, read))
+})
+
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
@@ -32,6 +49,7 @@ function Book(title, author, pages, read){
 
 
 function addBookToLibrary(book){
-
+    myLibrary.push(book);
+    console.log(myLibrary);
 }
 
