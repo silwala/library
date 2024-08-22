515a563e9b5129381ea8fd46ee3aaaf25d23dce6
Aditya Silwal <silwala@sheridancollege.ca>
2024-08-22 03:35:11 -0400

    add overlay when modal is open

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ const myLibrary = [];
 const modal = document.querySelector("#modal");
 const addButton = document.querySelector("#add-button");
 const closeModal = document.querySelector("#close-modal")
+const overlay = document.querySelector("#overlay")
+const library = document.querySelector("#library")
 
 addButton.addEventListener("click", () =>{
     modal.classList.add("active");
+    overlay.classList.add("active");
 })
 
 closeModal.addEventListener("click", () => {
     modal.classList.remove("active");
+    overlay.classList.remove("active");
 })
 
 function Book(title, author, pages, read){
